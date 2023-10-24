61d08c47181ed5dd4688111da827bce4bfde50d9
meiob <mokubo3@mail.ccsf.edu>
2023-10-24 03:30:10 -0700

    Added EOF(Ctrl+D) to exit other wise continue

---
+++ README.md
@@ -0,0 +1,59 @@
+# CS 110C Data Structures & Algorithm: C++ 71517-831
+> _Assignment 8 - Palindrome_ \
+> Palindrome Check using CharStack and CharQueue \
+> __Author:__ 🌻 Mei Okubo 🌻
+
+## 🐝 Basic Requirements
+- Make your own queue class that handles char data. - No templates needed.
+- Implement either an array-based or link-based queue. Your choice.
+- Create a function with a loop that:
+- Pushes each char onto a stack
+- Enqueues each char onto your queue
+- Compare the chars from the stack and queue to check if the string is a palindrome.
+
+## 🤟 Extra Credit
+- Use an array-based implementation with no extra data members like a count or isFull.
+- Sacrifice one array location to make front the index before the actual front.
+- Full if front equals (back+1) % (MAX_QUEUE+1), empty if front equals back.
+
+## 🔤 Implimentations
+1. Initialize instances of CharStack and CharQueue.
+2. Loop through the string S. For each character:
+   - Push it onto the CharStack
+   - Enqueue it onto the CharQueue
+3. Compare characters:
+   - Pop a character from CharStack
+   - Dequeue a character from CharQueue
+   - If they're ever not equal, S isn't a palindrome
+CharQueue Implementation Highlights:
+- Doesn't rely on item count or boolean isFull to track its fullness
+- Utilizes the array size MAX_QUEUE from Constants.h, ensuring clarity and ease of modification
+Pointer Initialization:
+- Front and back pointers are initialized to 0
+Enqueue Operation:
+1. Before enqueuing, check if the queue is full using isFull()
+2. Increment the back pointer using the modulo operation for wrapping
+3. Store the value at the incremented back pointer
+Dequeue Operation:
+1. Before dequeuing, check if the queue is empty using isEmpty()
+2. Increment the front pointer using the modulo operation
+3. Return the value from the front position
+Check if Queue is Full:
+- The queue is full if (back + 1) % MAX_QUEUE is equal to front
+Check if Queue is Empty:
+- The queue is empty if back is equal to front
+
+### ToDo
+☑️ Basic Requirments \
+☑️ Extra Credit
+
+
+### Notes:
+I mssied deadline due to recovery from illuness. \
+After complete basic requirments and extra credit modification, \
+I wrapped my code with loop while use input charcter(s) \
+and exit with EOF (Ctrl+D) and tested alright. \
+Before I had this loop, user needed to excute program each one entry at the time. \
+Now user can continue check palindromes! 🎉
+
+ℳ
--- main.cpp
+++ main.cpp
@@ -2,46 +2,10 @@
 // CS 110C Data Structures & Algorithm: C++ 71517-831
 // Assignment 8 - Palindrome
 // 🌻 Mei Okubo 🌻
-//
-// Palindrome Check using CharStack and CharQueue:
-//
-// Process:
-// 1. Initialize instances of CharStack and CharQueue.
-// 2. Loop through the string S. For each character:
-//    - Push onto the CharStack.
-//    - Enqueue onto the CharQueue.
-// 3. Compare characters:
-//    - Pop a character from CharStack.
-//    - Dequeue a character from CharQueue.
-//    - If they're ever not equal, S isn't a palindrome.
-//
-// CharQueue Implementation Highlights:
-// - Doesn't rely on item count or boolean isFull to track its fullness.
-// - Utilizes array size MAX_QUEUE from Constants.h, ensuring clarity and ease of modification.
-//
-// Pointer Init:
-// - Front and back pointers are initialized to 0.
-//
-// Enqueue:
-// 1. Before enqueuing, checks if full via isFull().
-// 2. Increments back pointer with modulo for wrapping.
-// 3. Value is stored at the incremented back pointer.
-//
-// Dequeue:
-// 1. Before dequeuing, checks if empty via isEmpty().
-// 2. Increments front pointer with modulo.
-// 3. Returns value from the incremented front position.
-//
-// Full Queue Check:
-// - Queue is full if (back + 1) % MAX_QUEUE is equal to front.
-//
-// Empty Queue Check:
-// - Queue is empty if back is equal to front.
 
 #include "CharQueue.h"
 #include "CharStack.h"
 #include <iostream>
-#include <cstring> // for strcpy()
 
 using namespace std;
 
@@ -49,22 +13,28 @@ bool isPalindrome(string S);  // Assuming you've implemented this elsewhere
 
 int main() {
     string str;
-    cout << "Enter a string to check if it's a palindrome: ";
+    while (true) {
+        cout << "Enter a string to check if it's a palindrome. " << endl;
+        cout << "(To quit, press Ctrl+D)" << endl;
+        cout << "> ";
+        char ch;
+        str.clear(); // Clear the previous input
+        while (cin.get(ch) && ch != '\n') { // Read a character at a time until Enter is pressed
+            str += ch;
+        }
 
-    // Instead of cin, we'll use getline to handle spaces as well
-    getline(cin, str);
+        if (cin.eof()) {
+        // If getline returns false, it means Ctrl+D was pressed (EOF)
+            break;
+        }
 
-    // Create a character array of sufficient size (+1 for the null terminator)
-    char charArray[str.size() + 1];
-
-    // Copy the string to the character array
-    strcpy(charArray, str.c_str());
-
-    if(isPalindrome(charArray)) { // Here we pass the char array instead of string
-        cout << "Yep, that's a palindrome!" << endl;
-    } else {
-        cout << "Nah, not a palindrome." << endl;
+        if(isPalindrome(str)) { // Here we pass the char array instead of string
+            cout << "Yep, that's a palindrome!" << endl;
+        } else {
+            cout << "Nah, not a palindrome." << endl;
+        }
     }
 
+    cout << "Bye Bye 🤟" << endl;
     return 0;
 }
